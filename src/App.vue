745083e12e1a77f26d15ae9fbbcdbdcd369e2e42
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-07-31 17:37:44 +0700

    Finishing Reaction Timer Game

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
 <h1>Timer Reaction Bone</h1>
+<button @click="start" :disabled="isPlaying">Play</button>
+<Block v-if="isPlaying" :delay="delay" @akhir="endGame" />
+<Result v-if="showResult" :score="score" />
 </template>
 
 <script>
+// Import component
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
   components: {
+    Block,
+    Result
+  },
+  data (){
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods: {
+    // When Click the button
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      
+      // Restart the score
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+
+      // Stop the game
+      this.isPlaying = false
+
+      // Menampilkan skor
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -19,5 +54,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Waktu Reaksi - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = 'Mantap Banget'
+        } else if (this.score < 400){
+            this.rank = 'Hebat Juga'
+        } else {
+            this.rank = 'Lebih Cepat Lagi'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
